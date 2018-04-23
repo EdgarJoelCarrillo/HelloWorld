687e866c697db60244e28f5fbaa2fdd3b680afee
DESKTOP-LNFBUNG\edgar <edgarjc65@gmail.com>
2018-04-23 13:21:09 -0500

    Aregge un php

---
+++ holamundo1.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>
+			Mi primera app en php
+	</title>
+
+	</head>
+	<body>
+		<h1>HOLA MUNDOOOOOOOOO</h1>
+		<?php 
+		$Base = 15;
+		$Altura = 12;
+		$Area = ($Base * $Altura)/2;
+
+		$Basecua = 10;
+		$Alturacua = 5;
+		$Areacua = ($Basecua * $Alturacua)/2;
+
+		print("Cuadrado Base: $Basecua <br> Altura: $Alturacua <br>");
+		print("El area Rectangulo  es: $Areacua");
+		print("<br>Triangulo Base: $Base <br> Altura: $Altura <br>"); 
+		print("El area del triangulo es: $Area"); 
+		
+		?>
+	</body>
+
+
+
+
+</html>
